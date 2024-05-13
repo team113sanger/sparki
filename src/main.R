dc6a46d63c3a4b4b53a1ecd68796d2b46e1196f3
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2024-05-13 13:51:13 +0100

    Started working on main.R

---
+++ src/main.R
@@ -0,0 +1,12 @@
+process_kraken2 <- function(
+    std_reports_path, mpa_reports_path,
+    metadata_path, ref_db_path
+) {
+
+    std_reports <- load_STDreports(std_reports_path)
+    mpa_reports <- load_MPAreports(mpa_reports_path)
+
+    
+
+
+}
