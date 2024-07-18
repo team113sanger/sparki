e3887b1c6ad3d96b0cec61aa88249a241a8f5fc1
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2024-07-18 13:45:34 +0100

    Developing a main for Sparki

--- src/main.R
+++ src/main.R
@@ -1,35 +1,84 @@
-process_kraken2 <- function(std_reports_path, mpa_reports_path, metadata_path, ref_db_path) {
+#' PREPARE DATA
+#'
+#' @param std_reports_path Path to directory containing standard Kraken2 reports.
+#' @param mpa_reports_path Path to directory containing MPA-style reports.
+#' @param reference_path Path to file containing information on a Kraken2 reference database.
+#' @param metadata_path Path to metadata file.
+#' @param verbose Verbosity level.
+#' @return Processed data ready for downstream steps.
+#' @export
+prepare_data <- function(std_reports_path, mpa_reports_path, reference_path, metadata_path, verbose) {
 
-    ############################
-    # Check report directories #
-    ############################
-
-    report_paths <- c(std_reports_path, mpa_reports_path)
-    names(report_paths) <- c("STD", "MPA")
-
-    for (path in report_paths) {
-        check_report_directory(directory_path = path, report_format = names(report_paths)[report_paths == path])
+    # Check the integrity of the directories provided & read reports.
+    reports_paths <- list(
+        "std" = std_reports_path, 
+        "mpa" = mpa_reports_path
+    )
+    for (report_format in names(reports_paths)) {
+        reports_paths[[path]] <- check_report_directory(
+            dirpath = reports_paths[[path]], 
+            report_format = report_format
+        )
     }
 
-    #######################################
-    # Check metadata & reference DB files #
-    #######################################
+    # Check the integrity of the reference and metadata (if provided) files.
+    if (is.na(metadata_path)) warning("No metadata file has been provided.")
+    for (file_path in c(reference_path, metadata_path)) { 
+        check_file(file_path = file_path) 
+    }
 
-    check_reference(ref_db_path)
+    # Read standard reports.
+    std_reports <- load_STDreports(reports_paths[[1]], verbose = FALSE)
+    
+    # Read MPA-style reports.
+    mpa_reports <- load_MPAreports(reports_paths[[2]], verbose = FALSE)
+    
+    # Read reference database data.
+    ref_db <- loadReference(reference_path)
 
+    # Read metadata (if any).
+    mdata <- NA
+    if (!(is.na(metadata_path))) mdata <- loadMetadata(metadata_path)
+    
+    return(list(std_reports, mpa_reports, ref_db, mdata))
 
-    ########################
-    # Load Kraken2 reports #
-    ########################
+}
 
-    std_reports <- load_STDreports(std_reports_dir = std_reports_path, verbose = FALSE)
-    mpa_reports <- load_MPAreports(mpa_reports_dir = mpa_reports_path, verbose = FALSE)
+#' PROCESS DATA
+#'
+#' @param std_reports_path Path to directory containing standard Kraken2 reports.
+#' @param mpa_reports_path Path to directory containing MPA-style reports.
+#' @param reference_path Path to file containing information on a Kraken2 reference database.
+#' @param metadata_path Path to metadata file.
+#' @param verbose Verbosity level.
+#' @return Processed reports ready for downstream analysis.
+#' @export
+process_kraken2 <- function(
+    std_reports_path, 
+    mpa_reports_path, 
+    reference_path, 
+    metadata_path, 
+    metadata_columns, 
+    verbose
+) {
 
-    ###########################
-    # Process Kraken2 reports #
-    ###########################
+    prepared_data <- prepare_data(
+        std_reports_path = std_reports_path,
+        mpa_reports_path = mpa_reports_path,
+        reference_path = reference_path,
+        metadata_path = metadata_path,
+        verbose = verbose
+    )
 
+    std_reports <- prepared_data[[1]]
+    mpa_reports <- prepared_data[[2]]
+    ref_db <- prepared_data[[3]]
+    mdata <- prepared_data[[4]]
+    
+    mdata_columns <- c("Diagnosis_short", "Site_group")
 
+    mpa_reports <- addMetadata(mpa_reports, mdata, mdata_columns)
+    std_reports <- addMetadata(std_reports, mdata, mdata_columns)
 
 
 }
