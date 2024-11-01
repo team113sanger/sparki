4ec161333e56e211aef324409ee08564c4971441
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2024-11-01 13:04:06 +0000

    Working on tests for load_MPAreports and load_STDreports

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,3 +1,64 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#################################
+## Tests for load_MPAreports() ##
+#################################
+
+test_that("load_MPAreports() returns a dataframe with the appropriate dimensions", {
+
+  # Define the expected number of columns.
+  expected_number_of_columns <- 12
+
+  # Load the MPA-style reports and obtain the actual numbers of rows and columns.
+  out <- load_MPAreports(mpa_reports)
+  actual_number_of_rows <- nrow(out)
+  actual_number_of_columns <- ncol(out)
+
+  # Carry out tests.
+  expect_false(actual_number_of_rows == 0)
+  expect_false(actual_number_of_columns == 0)
+  expect_false(is.null(dim(out)))
+  expect_equal(actual_number_of_columns, expected_number_of_columns)
+
+})
+
+test_that("the columns in the dataframe returned by load_MPAreports() correspond to the appropriate classes", {
+
+  # Define the expected number of columns.
+  expected_number_of_columns <- 12
+
+  # Load the MPA-style reports and obtain the actual numbers of rows and columns.
+  out <- load_MPAreports(mpa_reports)
+  actual_number_of_rows <- nrow(out)
+  actual_number_of_columns <- ncol(out)
+
+  # Carry out tests.
+  expect_false(actual_number_of_rows == 0)
+  expect_false(actual_number_of_columns == 0)
+  expect_false(is.null(dim(out)))
+  expect_equal(actual_number_of_columns, expected_number_of_columns)
+
+})
+
+
+
+
+#################################
+## Tests for load_STDreports() ##
+#################################
+
+test_that("load_STDreports() returns a dataframe with the appropriate dimensions", {
+
+  # Define the expected number of columns.
+  expected_number_of_columns <- 9
+
+  # Load the MPA-style reports and obtain the actual numbers of rows and columns.
+  out <- load_MPAreports(mpa_reports)
+  actual_number_of_rows <- nrow(out)
+  actual_number_of_columns <- ncol(out)
+
+  # Carry out tests.
+  expect_false(actual_number_of_rows == 0)
+  expect_false(actual_number_of_columns == 0)
+  expect_false(is.null(dim(out)))
+  expect_equal(actual_number_of_columns, expected_number_of_columns)
+
 })
