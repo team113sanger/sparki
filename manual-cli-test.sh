c410f127d39077730cec5bb7b431e85bf4fa63e0
Ian Vermes <iv3@sanger.ac.uk>
2024-11-15 17:04:56 +0000

    Add manual CLI test script and unit tests for process_kraken2

---
+++ manual-cli-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eou pipefail
+
+OUTDIR="manual-outdir"
+rm -rf $OUTDIR
+mkdir -p $OUTDIR
+
+Rscript -e "devtools::load_all(); SPARKI::cli()"\
+  --std-reports 'tests/testthat/testdata/std_reports/case_with_usual_reports' \
+  --mpa-reports 'tests/testthat/testdata/mpa_reports/case_with_usual_reports' \
+  --reference 'tests/testthat/testdata/inspect.txt' \
+  --organism 'Homo sapiens' \
+  --domain 'Viruses,Bacteria' \
+  --outdir "${OUTDIR}"
---
+++ tests/testthat/test-unit--main.R
@@ -0,0 +1,46 @@
+
+test_that("process_kraken2 - required options", {
+    # Setup
+    std_reports_path <- get_test_std_report_dir("usual")
+    mpa_reports_path <- get_test_mpa_report_dir("usual")
+    organism <- "Homo sapiens"
+    reference_path <- get_test_reference()
+    metadata_path <- NA
+    metadata_sample_col <- NA
+    metadata_columns <- NA
+    outdir_path <- get_local_tmp_dir()
+    prefix <- "foo"
+    verbose <- TRUE
+    include_eukaryotes <- FALSE
+    include_sample_names <- FALSE
+    domain <- "Viruses,Bacteria"
+    remove <- NA
+
+    # Check the outdir is empty
+    initial_outdir_size <- length(list.files(outdir_path))
+    expect_equal(initial_outdir_size, 0)
+
+
+    # Build the command and get the expected version
+    SPARKI::process_kraken2(
+        std_reports_path,
+        mpa_reports_path,
+        organism,
+        reference_path,
+        metadata_path,
+        metadata_sample_col,
+        metadata_columns,
+        outdir_path,
+        prefix,
+        verbose,
+        include_eukaryotes,
+        include_sample_names,
+        domain,
+        remove
+    )
+
+    # Assert that the outdir size is non-zero
+    final_outdir_size <- length(list.files(outdir_path))
+    expect_gt(final_outdir_size, 0)
+    }
+)
