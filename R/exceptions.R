2d8896678fa9eba5a222387ba10b70b5749c20e2
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2024-12-09 15:58:26 +0000

    Improved error messages and the documentation of the functions.

--- R/exceptions.R
+++ R/exceptions.R
@@ -1,4 +1,4 @@
-#' CARRY OUT CHECKS ON A GIVEN DIRECTORY
+#' Carry out checks on a given directory
 #'
 #' This function takes the path to a given directory and performs a few checks:
 #' - It checks if the directory exists.
@@ -19,15 +19,21 @@
 check_directory <- function(dirpath, expectation) {
   # Check that the directory exists.
   if (!(dir.exists(dirpath))) {
-    stop(EXCEPTIONS_CHECKDIR_DOES_NOT_EXIST, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECKDIR_DOES_NOT_EXIST, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # If the directory is not supposed to be empty, check that the directory is not empty.
   } else if (expectation == "not_empty" && length(list.files(dirpath)) == 0) {
-    stop(EXCEPTIONS_CHECKDIR_NOT_EMPTY, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECKDIR_NOT_EMPTY, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # If the directory is supposed to be empty, check that the directory is empty.
   } else if (expectation == "empty" && length(list.files(dirpath)) > 0) {
-    stop(EXCEPTIONS_CHECKDIR_EMPTY, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECKDIR_EMPTY, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # Ensure that the directory path has a slash at the end.
   } else if (substr(dirpath, nchar(dirpath), nchar(dirpath)) != "/") {
@@ -37,7 +43,7 @@ check_directory <- function(dirpath, expectation) {
   return(dirpath)
 }
 
-#' CARRY OUT CHECKS ON DIRECTORIES CONTAINING SPECIFICALLY STANDARD OR MPA-STYLE REPORTS
+#' Carry out checks on directories that contain standard or MPA-style reports
 #'
 #' This function takes the path to a directory containing either standard or
 #' MPA-style reports and performs a few checks:
@@ -70,25 +76,45 @@ check_report_directory <- function(dirpath, report_format) {
 
   # Check that the directory contains MPA reports if it is supposed to.
   if (report_format == "mpa" && n_mpa_reports == 0) {
-    stop(EXCEPTIONS_CHECK_REPORT_DIR_NO_MPA, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_NO_MPA, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
+
+  # Check that the MPA-style reports directory does not contain standard reports.
+  } else if (report_format == "mpa" && n_std_reports > 0) {
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_STD_IN_MPA, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # Check that the directory contains standard reports if it is supposed to.
   } else if (report_format == "std" && n_std_reports == 0) {
-    stop(EXCEPTIONS_CHECK_REPORT_DIR_NO_STD, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_NO_STD, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
+
+  # Check that the standard reports directory does not contain MPA-style reports.
+  } else if (report_format == "std" && n_mpa_reports > 0) {
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_MPA_IN_STD, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # Ensure that the report format specified is valid.
   } else if (!(report_format %in% c("std", "mpa"))) {
-    stop(EXCEPTIONS_CHECK_REPORT_DIR_INVALID, report_format)
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_INVALID, glue::double_quote(report_format))
+    logger::log_fatal(error_message)
+    stop(error_message)
 
   # Check that the directory does not contain both report formats.
   } else if (n_mpa_reports > 0 && n_std_reports > 0) {
-    stop(EXCEPTIONS_CHECK_REPORT_DIR_BOTH_FORMATS, dirpath)
+    error_message <- paste0(EXCEPTIONS_CHECK_REPORT_DIR_BOTH_FORMATS, glue::double_quote(dirpath))
+    logger::log_fatal(error_message)
+    stop(error_message)
   }
 
   return(dirpath)
 }
 
-#' CARRY OUT CHECKS ON A GIVEN FILE
+#' Carry out checks on a given file
 #'
 #' This function takes the path to a given file and performs a few checks:
 #' 1) First, it checks if the file exists.
@@ -100,11 +126,19 @@ check_report_directory <- function(dirpath, report_format) {
 #' check_file(file_path = "path/to/file")
 #'
 check_file <- function(file_path) {
-  if (!(file.exists(file_path))) stop(EXCEPTIONS_CHECKFILE_DOES_NOT_EXIST, file_path)
-  else if (file.size(file_path) == 0L) stop(EXCEPTIONS_CHECKFILE_EMPTY, file_path)
+  if (!(file.exists(file_path))) {
+    error_message <- paste0(EXCEPTIONS_CHECKFILE_DOES_NOT_EXIST, glue::double_quote(file_path))
+    logger::log_fatal(error_message)
+    stop(error_message)
+  }
+  else if (file.size(file_path) == 0L) {
+    error_message <- paste0(EXCEPTIONS_CHECKFILE_EMPTY, glue::double_quote(file_path))
+    logger::log_fatal(error_message)
+    stop(error_message)
+  }
 }
 
-#' CHECK IF A SET OF COLUMN NAMES IS PRESENT IN A GIVEN DATAFRAME
+#' Check if a set of column names is present in a given dataframe
 #'
 #' This function takes a dataframe and a vector containing column names that
 #' are expected to be found in the given dataframe; then it checks if each
@@ -115,15 +149,19 @@ check_file <- function(file_path) {
 #'  in the given dataframe.
 #'
 #' @examples
-#' check_columns(df = df, columns = c("col1", "col2", "col3"))
+#' check_columns(df = my_dataframe, columns = c("col1", "col2", "col3"))
 #'
 check_columns <- function(df, columns) {
   for (column in columns) {
-    if (!(column %in% colnames(df))) stop(EXCEPTIONS_CHECK_COLUMNS, column)
+    if (!(column %in% colnames(df))) {
+      error_message <- paste0(EXCEPTIONS_CHECK_COLUMNS, glue::double_quote(column))
+      logger::log_fatal(error_message)
+      stop(error_message)
+    }
   }
 }
 
-#' CARRY OUT CHECKS ON A PREFIX
+#' Carry out checks on a prefix
 #'
 #' This function takes a prefix and performs a few checks:
 #' 1) First, it checks if the prefix corresponds to the appropriate type ("character").
@@ -136,13 +174,19 @@ check_columns <- function(df, columns) {
 #' check_prefix(prefix = "prefix")
 #'
 check_prefix <- function(prefix) {
-  if (!(is.character(prefix))) stop(EXCEPTIONS_CHECK_PREFIX_NOT_CHARACTER, typeof(prefix))
+  if (!(is.character(prefix))) {
+    error_message <- paste0(EXCEPTIONS_CHECK_PREFIX_NOT_CHARACTER, glue::double_quote(typeof(prefix)))
+    logger::log_fatal(error_message)
+    stop(error_message)
+  }
   else if (substr(prefix, nchar(prefix), nchar(prefix)) != "_") prefix <- paste0(prefix, "_")
+
+  logger::log_trace("")
 
   return(prefix)
 }
 
-#' CHECK IF THE DOMAIN SPECIFIED IS VALID
+#' Check if the domain specified is valid
 #'
 #' This function takes a domain and checks if it is valid, i.e. one of "Viruses",
 #' "Bacteria", "Archaea", or "Eukaryota".
@@ -154,18 +198,36 @@ check_prefix <- function(prefix) {
 #'
 check_domain <- function(domain) {
   if (!(domain %in% c("Viruses", "Bacteria", "Archaea", "Eukaryota"))) {
-    stop(EXCEPTIONS_CHECK_DOMAIN_NOT_VALID, domain)
+    error_message <- paste0(EXCEPTIONS_CHECK_DOMAIN_NOT_VALID, glue::single_quote(domain))
+    logger::log_fatal(error_message)
+    stop(error_message)
   }
 }
 
+#' Check if the organism specified is present in the reports
+#'
+#' This function takes an organism at the species level (e.g. "Homo sapiens")
+#' and checks if it is present in the standard and MPA-style reports.
+#'
+#' @param organism The name of the organism being analysed (species-level). For
+#'  example, if your input data for Kraken2 comes from human samples, then the
+#'  organism should be "Homo sapiens".
+#'
+#' @examples
+#' check_organism(organism = "Homo sapiens")
+#'
 check_organism <- function(std_reports_path, mpa_reports_path, organism) {
-  std_reports <- load_STDreports(std_reports_path, verbose = FALSE)
+  std_reports <- load_STDreports(std_reports_path)
   if (!(organism %in% std_reports[[COLNAME_STD_TAXON]])) {
-    stop(EXCEPTIONS_CHECK_ORGANISM_STD, organism)
+    error_message <- paste0(EXCEPTIONS_CHECK_ORGANISM_STD, glue::double_quote(organism))
+    logger::log_fatal(error_message)
+    stop(error_message)
   }
 
-  mpa_reports <- load_MPAreports(mpa_reports_path, verbose = FALSE)
+  mpa_reports <- load_MPAreports(mpa_reports_path)
   if (!(organism %in% mpa_reports[[COLNAME_MPA_SPECIES]])) {
-    stop(EXCEPTIONS_CHECK_ORGANISM_MPA, organism)
+    error_message <- paste0(EXCEPTIONS_CHECK_ORGANISM_STD, glue::double_quote(organism))
+    logger::log_fatal(error_message)
+    stop(error_message)
   }
 }
