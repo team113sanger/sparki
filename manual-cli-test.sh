9e3344f79e008130bf5244141324e05686949579
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2025-03-04 14:33:03 +0000

    Updated paths to reflect changes made in the testdata directory

--- manual-cli-test.sh
+++ manual-cli-test.sh
@@ -5,11 +5,11 @@ OUTDIR="manual-outdir"
 rm -rf ${OUTDIR}
 mkdir -p ${OUTDIR}
 
-Rscript -e "devtools::load_all(); SPARKI::cli()"\
-  --std-reports 'tests/testthat/testdata/std_reports/case_with_usual_reports' \
-  --mpa-reports 'tests/testthat/testdata/mpa_reports/case_with_usual_reports' \
+Rscript -e "devtools::load_all(); SPARKI::cli()" \
+  --std-reports 'tests/testthat/testdata/std_reports/valid_reports' \
+  --mpa-reports 'tests/testthat/testdata/mpa_reports/valid_reports' \
   --reference 'tests/testthat/testdata/inspect.txt' \
   --organism 'Homo sapiens' \
   --domain 'Viruses,Bacteria' \
   --outdir "${OUTDIR}" \
-  --verbosity trace
+  --verbosity debug
