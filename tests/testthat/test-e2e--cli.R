25a2d6c1121c790ce6ab62c50c3c9d0beb3f4380
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2025-03-04 14:53:53 +0000

    Added some tests for 'cli() --version' and 'cli() --help'

--- tests/testthat/test-e2e--cli.R
+++ tests/testthat/test-e2e--cli.R
@@ -1,3 +1,55 @@
+test_that("It is possible to retrieve the package version through cli()", {
+    logger::log_threshold(logger::OFF)
+
+    rscript <- Sys.which("Rscript")
+
+    # Build CLI command.
+    cli_cmd <- paste(
+        rscript, "-e", glue::single_quote("devtools::load_all(quiet = TRUE); SPARKI::cli()"),
+        "--version", sep = " "
+    )
+
+    # Launch the CLI and check that the exit code is zero (i.e. success!).
+    exit_code <- system(
+        command = cli_cmd,
+        ignore.stdout = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
+        ignore.stderr = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
+        intern = FALSE
+    )
+    expect_equal(exit_code, 0)
+
+    # Launch the CLI again, now capturing the output.
+    out <- system(
+        command = cli_cmd,
+        ignore.stdout = FALSE,
+        ignore.stderr = FALSE,
+        intern = TRUE
+    )
+    expect_true(grepl(packageVersion("SPARKI"), out))
+})
+
+test_that("It is possible to get help through cli()", {
+    logger::log_threshold(logger::OFF)
+
+    rscript <- Sys.which("Rscript")
+
+    # Build CLI command.
+    cli_cmd <- paste(
+        rscript, "-e", glue::single_quote("devtools::load_all(quiet = TRUE); SPARKI::cli()"),
+        "--help", sep = " "
+    )
+
+    # Launch the CLI and check that the exit code is zero (i.e. success!).
+    exit_code <- system(
+        command = cli_cmd,
+        ignore.stdout = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
+        ignore.stderr = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
+        intern = FALSE
+    )
+    expect_equal(exit_code, 0)
+})
+
+
 test_that("cli() works when only required arguments are specified", {
     logger::log_threshold(logger::OFF)
 
@@ -16,13 +68,13 @@ test_that("cli() works when only required arguments are specified", {
         sep = " "            # the log messages will exist but they will not be printed out (as defined below).
     )
 
+    # Launch the CLI and check that the exit code is zero (i.e. success!).
     exit_code <- system(
         command = cli_cmd,
         ignore.stdout = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         ignore.stderr = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         intern = FALSE
     )
-
     expect_equal(exit_code, 0)
 })
 
@@ -44,13 +96,13 @@ test_that("cli() works when the report contains white space-delimited species na
         sep = " "            # the log messages will exist but they will not be printed out (as defined below).
     )
 
+    # Launch the CLI and check that the exit code is zero (i.e. success!).
     exit_code <- system(
         command = cli_cmd,
         ignore.stdout = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         ignore.stderr = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         intern = FALSE
     )
-
     expect_equal(exit_code, 0)
 })
 
@@ -72,12 +124,12 @@ test_that("cli() fails when the user-defined species's delimiter is neither a wh
         sep = " "            # the log messages will exist but they will not be printed out (as defined below).
     )
 
+    # Launch the CLI and check that the exit code is not zero (i.e. failure).
     exit_code <- system(
         command = cli_cmd,
         ignore.stdout = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         ignore.stderr = TRUE, # Ignoring this as we do not want log messages to be printed out during the test.
         intern = FALSE
     )
-
     expect_gt(exit_code, 0)
 })
